abc5bda6bc2d0b13d5225e7e112905b1178ebaee
kirill-korolev <kkorolev@ozon.ru>
2021-04-09 02:18:12 +0300

    Contest 1 J

--- algo2/contest1/J.cpp
+++ algo2/contest1/J.cpp
@@ -1,47 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
+#include <queue>
 
 using namespace std;
 
 struct Graph {
-    Graph(int n, int m) : n_rows(n), n_cols(m), visited(n * m), source(n * m, -1), h(n * m) {}
+    Graph(int n, int m) : n_rows(n), n_cols(m), visited(n * m), h(n * m) {}
 
-    inline int idx(int x, int y) {
-        return x * n_rows + y;
+    inline pair<int, int> coords(int idx) {
+        return make_pair(idx / n_cols, idx % n_cols);
     }
 
-    int dfs(int x, int y) {
+    void dfs(int v) {
         static vector<int> dx = {-1, 0, 0, 1};
         static vector<int> dy = {0, -1, 1, 0};
 
-        visited[idx(x, y)] = 1;
-        source[idx(x, y)] = idx(x, y);
+        visited[v] = 1;
 
         for (int d = 0; d < 4; ++d) {
+            auto [x, y] = coords(v);
             int tx = x + dx[d];
             int ty = y + dy[d];
 
             if (tx >= 0 && tx < n_rows && ty >= 0 && ty < n_cols) {
-                if (h[idx(x, y)] >= h[idx(tx, ty)]) {
-                    if (!visited[idx(tx, ty)])
-                        source[idx(x, y)] = dfs(tx, ty);
-                    else
-                        source[idx(x, y)] = source[idx(tx, ty)];
+                int u = tx * n_cols + ty;
+
+                if (!visited[u] && h[v] <= h[u]) {
+                    dfs(u);
                 }
             }
         }
-
-        if (source[idx(x, y)] == idx(x, y)) {
-            h[idx(x, y)] = -1;
-        }
-
-        return source[idx(x, y)];
     }
 
     int n_rows;
     int n_cols;
-    vector<int> source;
+
     vector<int> visited;
     vector<int> h;
 };
@@ -52,31 +45,28 @@ int main() {
     cin >> n >> m;
     Graph g(n, m);
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            cin >> g.h[i * n + j];
-        }
+    using Square = pair<int, int>;
+    auto cmp = [](const Square& lhs, const Square& rhs) { return lhs.second > rhs.second; };
+    priority_queue<Square, vector<Square>, decltype(cmp)> q(cmp);
+
+    for (int idx = 0; idx < n * m; ++idx) {
+        cin >> g.h[idx];
+        q.emplace(idx, g.h[idx]);
     }
 
+    int num_targets = 0;
+    while (!q.empty()) {
+        auto [v, _] = q.top();
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (!g.visited[i * n + j]) {
-                g.dfs(i, j);
-                /*
-                for (int k = 0; k < n; ++k) {
-                    for (int l = 0; l < m; ++l) {
-                        cout << "(" << g.source[k * n + l] / n << ", " << g.source[k * n + l] % m << ") ";
-                    }
-                    cout << "\n";
-                }
-                cout << "\n\n";*/
-            }
+        if (!g.visited[v]) {
+            g.dfs(v);
+            ++num_targets;
         }
+
+        q.pop();
     }
 
-    unordered_set<int> unique_sources(g.source.begin(), g.source.end());
-    cout << unique_sources.size() << "\n";
+    cout << num_targets << "\n";
 
     return 0;
 }
