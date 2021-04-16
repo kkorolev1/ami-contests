a6b221ac40f6d7a686f39735632492283d37e102
Kirill <kakorolev@edu.hse.ru>
2021-04-17 01:52:09 +0300

    Contest 3.2

--- algo2/contest3/D.cpp
+++ algo2/contest3/D.cpp
@@ -11,70 +11,64 @@ int main() {
     ios::ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    struct Vertex {
-        int idx, x, y;
+    using ll = long long;
+    using Vec2 = pair<int, int>;
 
-        bool operator==(const Vertex& other) const {
-            return idx == other.idx && x == other.x && y == other.y;
-        }
-
-        size_t operator()(const Vertex& v) const {
-            return hash<int>()(v.idx) ^ hash<int>()(1000 * v.x + v.y);
+    struct Vec2Hash {
+        size_t operator()(const Vec2& v) const {
+            return hash<int>()(1000 * v.first + v.second);
         }
     };
 
-    int num_rows, num_cols, target_x, target_y, num_flees;
-    cin >> num_rows >> num_cols >> target_x >> target_y >> num_flees;
+    int num_rows, num_cols, num_flees;
+    Vec2 target;
+    cin >> num_rows >> num_cols >> target.first >> target.second >> num_flees;
 
-    using ll = long long;
-    queue<Vertex> q;
-    unordered_set<Vertex, Vertex> visited;
-    unordered_map<Vertex, ll, Vertex> dist;
-    const long long MAX_VALUE = 1e9;
-    vector<ll> min_dist(num_flees, MAX_VALUE);
+    queue<Vec2> q;
+    unordered_set<Vec2, Vec2Hash> flees;
+    unordered_set<Vec2, Vec2Hash> visited;
+    unordered_map<Vec2, int, Vec2Hash> dist;
 
     for (int i = 0; i < num_flees; ++i) {
-        Vertex v{i};
-        cin >> v.x >> v.y;
-        q.push(v);
-        visited.insert(v);
+        int x, y;
+        cin >> x >> y;
+        flees.emplace(x, y);
     }
 
+    q.push(target);
+    visited.insert(target);
+
     vector<int> dx = {-2, -2, -1, -1, 1, 1, 2, 2};
-    vector<int> dy = {-1, 1, -2, 2, -1, 2, -1, 1};
+    vector<int> dy = {-1, 1, -2, 2, -2, 2, -1, 1};
     const int d_size = dx.size();
 
     while (!q.empty()) {
-        auto& v = q.front();
+        auto v = q.front();
         q.pop();
 
-        if (v.x == target_x && v.y == target_y) {
-            min_dist[v.idx] = min(min_dist[v.idx], dist[v]);
-            continue;
-        }
-
         for (int d = 0; d < d_size; ++d) {
-            int tx = v.x + dx[d];
-            int ty = v.y + dy[d];
+            int tx = v.first + dx[d];
+            int ty = v.second + dy[d];
 
             if (tx >= 1 && tx <= num_rows && ty >= 1 && ty <= num_cols) {
-                Vertex u{v.idx, tx, ty};
+                Vec2 u{tx, ty};
                 if (visited.find(u) == visited.end()) {
-                    dist[u] = dist[v] + 1;
                     visited.insert(u);
+                    dist[u] = dist[v] + 1;
                     q.push(u);
                 }
             }
         }
+
     }
 
     ll ans = 0;
-    for (ll d : min_dist) {
-        if (d == MAX_VALUE) {
+    for (auto& f : flees) {
+        if (visited.find(f) == visited.end()) {
             cout << "-1\n";
             return 0;
         }
-        ans += d;
+        ans += dist[f];
     }
 
     cout << ans << "\n";
